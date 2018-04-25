17728229d65b41dcfbe74a4dd20f526232905d68
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-04-25 15:02:44 +0200

    Incorporating Wiebe's requests

--- models/run_model.sh
+++ models/run_model.sh
@@ -6,5 +6,9 @@ mkdir outputs
 # run the model
 python hydrology_and_erosion_model.py
 
-# visualize the maps (output and input)
-aguila outputs/*.map inputs/*.map
+#~ # visualize the maps (output and input)
+#~ aguila outputs/*.map inputs/*.map
+
+# visualize the maps (output only)
+aguila outputs/*.map
+
